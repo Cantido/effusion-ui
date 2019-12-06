6e3aa67c691d0de42521e07e05b255c13cdf8254
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-12-06 12:18:41 -0600

    Create skeleton GraphQL Vue app

---
+++ src/components/Session.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="session">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Session',
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false
 
+Vue.use(VueApollo);
+const httpLink = new HttpLink({
+  uri: `http://localhost:4000/api`
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
 new Vue({
+  provide: apolloProvider.provide(),
   render: h => h(App),
 }).$mount('#app')
