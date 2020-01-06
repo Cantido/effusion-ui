3d9d068fd5560d5c081b805d4d1083e6691c7b67
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-01-06 16:52:25 -0600

    Extract the metadata upload component

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,60 +1,20 @@
 <template>
   <div class="session">
-    <span class="add-file">
-      <span>Add a file: </span>
-      <input type="file" ref="metadataFile" @change="selectedFile">
-      <button @click="uploadMetadata">Download</button>
-    </span>
-
+    <metadata-upload></metadata-upload>
     <torrents-table></torrents-table>
-
   </div>
 </template>
 
 <script>
-import gql from 'graphql-tag';
-import format from '../lib/format';
 import TorrentsTable from './TorrentsTable.vue';
+import MetadataUpload from './MetadataUpload.vue';
 
 export default {
   name: 'Session',
   components: {
-    TorrentsTable
+    TorrentsTable,
+    MetadataUpload
   },
-  data: () => {
-    return {
-      metadata: '',
-    }
-  },
-  methods: {
-    uploadMetadata: async function() {
-      const result = await this.$apollo.mutate({
-        mutation: gql`mutation ($meta: Binary!) {
-          addTorrent(meta:$meta) {
-            id,
-            name,
-            downloaded
-          }
-        }`,
-        variables: {
-          meta: this.metadata,
-        },
-      })
-    },
-    selectedFile: function() {
-
-      let file = this.$refs.metadataFile.files[0];
-      if(!file || file.type !== 'application/x-bittorrent') return;
-
-      let reader = new FileReader();
-      reader.readAsBinaryString(file);
-      reader.onload = evt => {
-        this.metadata = btoa(evt.target.result);
-      }
-      reader.onerror = () => {}
-    },
-    formatBytes: format.bytes,
-  }
 }
 </script>
 
