fd32f76a25a1b641ca554da89a4f6776a99f8f8a
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-01-06 16:53:24 -0600

    Remove unused functions

--- src/components/TorrentsTable.vue
+++ src/components/TorrentsTable.vue
@@ -52,32 +52,6 @@ export default {
     pollInterval: 300
   },
   methods: {
-    uploadMetadata: async function() {
-      const result = await this.$apollo.mutate({
-        mutation: gql`mutation ($meta: Binary!) {
-          addTorrent(meta:$meta) {
-            id,
-            name,
-            downloaded
-          }
-        }`,
-        variables: {
-          meta: this.metadata,
-        },
-      })
-    },
-    selectedFile: function() {
-
-      let file = this.$refs.metadataFile.files[0];
-      if(!file || file.type !== 'application/x-bittorrent') return;
-
-      let reader = new FileReader();
-      reader.readAsBinaryString(file);
-      reader.onload = evt => {
-        this.metadata = btoa(evt.target.result);
-      }
-      reader.onerror = () => {}
-    },
     formatBytes: format.bytes,
   }
 }
